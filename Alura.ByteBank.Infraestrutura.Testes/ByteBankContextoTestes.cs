83b9bf35392889a56af5df6f8321961f69d79d67
Igor Tudisco <igortudisco@hotmail.com>
2023-05-11 17:12:11 -0300

    Issue fixed - New tests have been created for testing

--- Alura.ByteBank.Infraestrutura.Testes/ByteBankContextoTestes.cs
+++ Alura.ByteBank.Infraestrutura.Testes/ByteBankContextoTestes.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using Alura.ByteBank.Dados.Contexto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Alura.ByteBank.Infraestrutura.Testes
 {
-    internal class ByteBankContextoTestes
+    
+    public class ByteBankContextoTestes
     {
+        [Fact]
+        public void TestaConexaoContextoTeste()
+        {
+            // Arrange
+            var contexto = new ByteBankContexto();
+            bool conectado;
+
+            // Act
+            try
+            {
+                conectado = contexto.Database.CanConnect();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Não foi possível se conectar ao banco de dados." + " " + e);
+            }
+
+            // Assert
+            Assert.True(conectado); 
+        }
+
     }
 }
